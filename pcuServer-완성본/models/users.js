38025fbed85be59a81c67c911af814dbaad7c706
dhksgml <69703890+dhksgml@users.noreply.github.com>
2021-09-30 20:30:53 +0900

    MySQL 시퀄라이즈를 사용해서 사용자 정보를 DB에 저장하기
    gameName, password, userId를 DB에 저장하도록 만들었습니다.
    어제 시퀄라이즈 무슨 에러가 났던 부분은 users.js에 컬럼 정의하는 부분을 return했더니 해결되었습니다.

---
+++ pcuServer-완성본/models/users.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize,DataTypes)=>{
+    return sequelize.define('user',{
+        gameName:{
+            type: DataTypes.STRING(40),
+            allowNull: true,
+        },
+        userId:{
+            type: DataTypes.STRING(40),
+            allowNull: true,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+    },{
+        timestamps: true,
+        paranoid: true,
+    })
+};
